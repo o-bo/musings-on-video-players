1f1099417ef77cb3be3fbf0872a565ef663925da
o-bo <olivier.berho@gmail.com>
2017-09-08 22:59:20 +0200

    [*] extract player in component.

--- src/App.js
+++ src/App.js
@@ -1,78 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import VideoPlayer from './components/VideoPlayer';
+
 import logo from './logo.svg';
 import './App.css';
 
-/* eslint-disable jsx-a11y/media-has-caption */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-class App extends Component {
-  state = {
-    currentProgress: 0,
-    totalProgress: 0,
-  }
+export default () => (
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Musings on video players with React</h2>
+    </div>
 
-  componentDidMount() {
-    this.videoPlayer.ontimeupdate = this.updateTime;
-  }
-
-  setProgression = (e) => {
-    const progressBoundingRect = this.progressBar.getBoundingClientRect();
-    const {
-      width,
-      left,
-    } = progressBoundingRect;
-    const offset = e.pageX - left;
-    const offsetInSeconds = (offset * this.videoPlayer.duration) / width;
-    this.videoPlayer.currentTime = offsetInSeconds;
-  }
-
-  updateTime = (prevState) => {
-    this.setState(() => ({
-      currentProgress: this.videoPlayer.currentTime,
-      totalProgress: prevState.totalProgress !== this.videoPlayer.duration
-        ? this.videoPlayer.duration
-        : prevState.totalProgress,
-    }));
-  }
-
-  playPause = () => {
-    if (this.videoPlayer.paused) {
-      this.videoPlayer.play();
-    } else {
-      this.videoPlayer.pause();
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Musings on video players with React</h2>
-        </div>
-
-        <div style={{ textAlign: 'center' }}>
-          <button onClick={this.playPause}>Play/Pause</button>
-          <progress
-            ref={(bar) => { this.progressBar = bar; }}
-            value={this.state.currentProgress}
-            max={this.state.totalProgress}
-            onClick={this.setProgression}
-          />
-          <video
-            ref={(player) => { this.videoPlayer = player; }}
-            width="640"
-            height="480"
-            autoPlay
-          >
-            <source src="https://s3-eu-west-1.amazonaws.com/onrewind-test-bucket/big_buck_bunny.mp4" type="video/mp4" />
-            Your browser is not aware.
-          </video>
-        </div>
-      </div>
-    );
-  }
-}
-/* eslint-enable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-enable jsx-a11y/media-has-caption */
-
-export default App;
+    <VideoPlayer
+      src="https://s3-eu-west-1.amazonaws.com/onrewind-test-bucket/big_buck_bunny.mp4"
+      type="video/mp4"
+    />
+  </div>
+);
---
+++ src/components/VideoPlayer.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+/* eslint-disable jsx-a11y/media-has-caption */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+class VideoPlayer extends Component {
+  static propTypes = {
+    src: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+  }
+
+  state = {
+    currentProgress: 0,
+    totalProgress: 0,
+  }
+
+  componentDidMount() {
+    this.videoPlayer.ontimeupdate = this.updateTime;
+  }
+
+  setProgression = (e) => {
+    const progressBoundingRect = this.progressBar.getBoundingClientRect();
+    const {
+      width,
+      left,
+    } = progressBoundingRect;
+    const offset = e.pageX - left;
+    const offsetInSeconds = (offset * this.videoPlayer.duration) / width;
+    this.videoPlayer.currentTime = offsetInSeconds;
+  }
+
+  updateTime = (prevState) => {
+    this.setState(() => ({
+      currentProgress: this.videoPlayer.currentTime,
+      totalProgress: prevState.totalProgress !== this.videoPlayer.duration
+        ? this.videoPlayer.duration
+        : prevState.totalProgress,
+    }));
+  }
+
+  playPause = () => {
+    if (this.videoPlayer.paused) {
+      this.videoPlayer.play();
+    } else {
+      this.videoPlayer.pause();
+    }
+  }
+
+  render() {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <button onClick={this.playPause}>Play/Pause</button>
+        <progress
+          ref={(bar) => { this.progressBar = bar; }}
+          value={this.state.currentProgress}
+          max={this.state.totalProgress}
+          onClick={this.setProgression}
+        />
+        <video
+          ref={(player) => { this.videoPlayer = player; }}
+          width="1080"
+          height="720"
+          autoPlay
+        >
+          <source
+            {...this.props}
+          />
+          Your browser is not aware.
+        </video>
+      </div>
+    );
+  }
+}
+/* eslint-enable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-enable jsx-a11y/media-has-caption */
+
+export default VideoPlayer;
