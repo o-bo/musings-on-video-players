3e643f9184ab70af091610c2ed00c8d971ef3ee9
o-bo <olivier.berho@gmail.com>
2017-09-09 20:04:43 +0200

    [+] test App.

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,33 @@
 /* eslint-disable no-undef */
 import React from 'react';
-import { shallow } from 'enzyme';
+import {
+  shallow,
+  mount,
+} from 'enzyme';
 
 import App from './App';
 
 it('renders without crashing', () => {
   shallow(<App />);
+});
+
+it('mounts without crashing', () => {
+  mount(<App />);
+});
+
+it('renders header', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.App-header')).toHaveLength(1);
+});
+
+it('renders logo', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.App-logo')).toHaveLength(1);
+});
+
+it('renders title', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('h2')).toHaveLength(1);
 });
 
 /* eslint-enable no-undef */
