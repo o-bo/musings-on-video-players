7d3ad2f58ba182c88523ae366967c3e168d7a468
o-bo <olivier.berho@gmail.com>
2017-09-09 20:33:38 +0200

    [+] test VideoContainer's initial state.

--- src/components/VideoContainer.test.js
+++ src/components/VideoContainer.test.js
@@ -12,3 +12,23 @@ it('renders player', () => {
   const wrapper = shallow(<VideoContainer {...videoParams} />);
   expect(wrapper.find('.video-container')).toHaveLength(1);
 });
+
+it('initializes state with isPlaying at TRUE', () => {
+  const wrapper = shallow(<VideoContainer {...videoParams} />);
+  expect(wrapper.state().isPlaying).toBeTruthy();
+});
+
+it('initializes state with currentProgress at 0', () => {
+  const wrapper = shallow(<VideoContainer {...videoParams} />);
+  expect(wrapper.state().currentProgress).toEqual(0);
+});
+
+it('initializes state with startTime at 0', () => {
+  const wrapper = shallow(<VideoContainer {...videoParams} />);
+  expect(wrapper.state().startTime).toEqual(0);
+});
+
+it('initializes state with totalProgress at 0', () => {
+  const wrapper = shallow(<VideoContainer {...videoParams} />);
+  expect(wrapper.state().totalProgress).toEqual(0);
+});
