18095224843ce43adff90a6633b1894be171cc23
o-bo <olivier.berho@gmail.com>
2017-09-09 19:51:56 +0200

    [*] rewrite base test using enzyme.

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
 });
 
 /* eslint-enable no-undef */
