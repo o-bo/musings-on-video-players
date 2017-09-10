2f893a8675485dd9d4318d98aa314aeac80a6834
o-bo <olivier.berho@gmail.com>
2017-09-10 11:03:35 +0200

    [*] reports and logs in gitignore
    [*] reports and logs in gitignore

--- nw/tests/researchOnGoogle.test.js
+++ nw/tests/researchOnGoogle.test.js
@@ -2,11 +2,8 @@ module.exports = {
   'Search on google': (browser) => {
     browser
       .init()
-      .waitForElementVisible('body', 1000)
-      .setValue('input[type=text]', browser.globals.searchTerm)
-      .waitForElementVisible('input[name=btnK]', 1000)
-      .click('input[name=btnK]')
-      .pause(1000)
+      .page.google().fillInSearchInput()
+      .page.google().submit()
       .assert.containsText('#main', browser.globals.movieName)
       .end();
   },
